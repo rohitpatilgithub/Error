6658bf683abbc386047ef228fdab4aac00b566d7
Rohit Patil <143780223+rohitpatilgithub@users.noreply.github.com>
2024-10-09 15:50:44 +0530

    Update remove.js

--- remove.js
+++ remove.js
@@ -1,7 +1,7 @@
 const getBtn = document.querySelector("button");
 
 getBtn.addEventListener("click", () => {
-    document.body.removeEventListener("dbclick",func1);
+    document.body.removeEventListener("dbclick",func1); // It isn't functioning
 });
 
 document.body.addEventListener("click",function func1 () {
